08c87fe251971e30f4acf0d2b30b9f2aed546929
kreasidikamala <suksesberdua747@gmail.com>
2026-09-24 16:08:36 +0800

    Isi konfigurasi Firebase Web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
-// Salin konfigurasi Web app dari Firebase Console > Project settings > Your apps.
-// Nilai ini memang digunakan di browser; perlindungan data ada pada Firestore Rules.
+// Konfigurasi Web app Firebase. Perlindungan data ada pada Firestore Rules.
 window.WEDDING_FIREBASE_CONFIG = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  appId: ''
+  apiKey: 'AIzaSyAn_aJkxL-_CuC0S9QC3UGujhxJlCBfdN8',
+  authDomain: 'undangan-218fe.firebaseapp.com',
+  projectId: 'undangan-218fe',
+  storageBucket: 'undangan-218fe.firebasestorage.app',
+  messagingSenderId: '120019636020',
+  appId: '1:120019636020:web:c27a0cfb89672e1b249f61',
+  measurementId: 'G-8GDHFPQS2E'
 };
